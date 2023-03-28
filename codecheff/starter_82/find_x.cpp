411710da43e94dfe85371c48bda71261d8a3c815
TheMrAI <mr.istvan.acs@gmail.com>
2023-03-28 13:28:20 +0200

    Add codeforces 860 c

---
+++ codecheff/starter_82/find_x.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+auto main() -> int {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    auto t = 0;
+    cin>>t;
+    while(t--) {
+	    auto a=0;
+	    auto b=0;
+	    auto c=0;
+	    auto d=0;
+	    cin>>a;
+	    cin>>b;
+	    cin>>c;
+	    cin>>d;
+	    
+	    auto x=0;
+	    a = a%b;
+	    c = c%d;
+	    
+	    auto diff_ba = b-a;
+	    auto diff_dc = d-c;
+	    
+	    if (diff_ba > 1 && diff_dc > 1) {
+	        x = 1;
+        } else if (diff_ba == diff_dc) {
+            x = 1;
+	    } else {
+	        x = ((b*d) / gcd(b, d)) - (a%b);
+	    }
+	    cout<<x<<endl;
+	}
+	return 0;
+}
---
+++ codeforces/860/c.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+auto main() -> int {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    auto t = 0;
+    cin>>t;
+    while(t--) {
+        int64_t n = 0;
+        cin>>n;
+        vector<pair<int64_t, int64_t>> candies;
+        for(auto i=0; i<n; ++i) {
+            int64_t a = 0;
+            int64_t b = 0;
+            cin>>a;
+            cin>>b;
+            candies.emplace_back(make_pair(a,b));
+        }
+ 
+        auto prev_gcd = candies[0].first * candies[0].second;
+        auto prev_lcm = candies[0].second;
+        auto individual_price_tags = 1;
+        for (auto i=int64_t{1}; i<n; ++i) {
+            auto multiple = candies[i].first * candies[i].second;
+            auto new_gcd = gcd(prev_gcd, multiple);
+            auto new_lcm = lcm(prev_lcm, candies[i].second);
+            
+            if(new_gcd % new_lcm == 0) {
+                prev_gcd = new_gcd;
+                prev_lcm = new_lcm;
+            } else {
+                prev_gcd = multiple;
+                prev_lcm = candies[i].second;
+                ++individual_price_tags;
+            }
+        }
+        cout<<individual_price_tags<<endl;
+    }
+ 
+
+    return 0;
+}
