8619d9670af5eb7b73df1ce916bff8cfdca16e51
TheMrAI <mr.istvan.acs@gmail.com>
2023-03-28 19:28:31 +0200

    Add Codecheff Make them alike

---
+++ template.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+auto main() -> int {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    auto t=int64_t{0};
+    cin>>t;
+    while(t--) {
+        auto n=int64_t{0};
+        cin>>n;
+
+    }
+
+    return 0;
+}
