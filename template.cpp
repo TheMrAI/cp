d61253fd446c9e2c88044b3eed0331faf1cc36f2
TheMrAI <mr.istvan.acs@gmail.com>
2023-04-03 09:00:03 +0200

    Add ceil2 function

--- template.cpp
+++ template.cpp
@@ -3,6 +3,12 @@
 using namespace std;
 using i6 = int64_t;
 
+// Source: https://codeforces.com/blog/entry/78852
+int ceil2(i6 a, i6 b) {
+    if (a == 0) return 0;
+    return ((a - 1)/b) + 1;
+}
+
 auto main() -> int {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
