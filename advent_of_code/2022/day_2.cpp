a0c8930f1a8292642375ea2b30f42e872b78d04d
TheMrAI <mr.istvan.acs@gmail.com>
2023-04-04 08:50:36 +0200

    Add Advent of Code 2022 first 7 solutions

---
+++ advent_of_code/2022/day_2.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+// A - Rock
+// B - Paper
+// C - Scissors
+
+// X - Rock
+// Y - Paper
+// Z - Scissors
+
+// X - Lose
+// Y - Draw
+// Z - Win
+
+auto get_score(char opponent, char you) -> uint64_t {
+    auto opponent_value = static_cast<uint64_t>(opponent - 'A');
+    auto you_value = static_cast<uint64_t>(you - 'X');
+
+    auto score = you_value + 1;
+    auto diff = static_cast<int64_t>(you_value) - static_cast<int64_t>(opponent_value);
+    if (diff == 0) {
+        score += 3;
+    } else if(diff == 1 || diff == -2) {
+        score += 6;
+    }
+
+    return score;
+}
+
+auto get_updated_score(char opponent, char result) -> uint64_t {
+    auto opponent_value = static_cast<uint64_t>(opponent - 'A') + 1;
+
+    auto score = 0;
+    if (result == 'Y') {
+        score += 3 + opponent_value;
+    } else if (result == 'Z') {
+        score += 6;
+        if (opponent_value == 3) {
+            score += 1;
+        }
+        else {
+            score += opponent_value + 1;
+        }
+    } else {
+        if (opponent_value == 1) {
+            score += 3;
+        } else {
+            score += opponent_value - 1;
+        }
+    }
+
+    return score;
+}
+
+auto main() -> int {
+    auto total_score = uint64_t{0};
+    auto updated_score = uint64_t{0};
+
+    auto input = std::string{};
+    while (std::getline(std::cin, input)) {
+        total_score += get_score(input[0], input[2]);
+        updated_score += get_updated_score(input[0], input[2]);
+    }
+    std::cout << "Total score: " << total_score << std::endl;
+    std::cout << "Updated score: " << updated_score << std::endl;
+
+    return 0;
+}
---
+++ advent_of_code/2022/day_5.cpp
@@ -0,0 +1,84 @@
+#include <iostream>
+#include <regex>
+#include <stack>
+#include <vector>
+
+auto main() -> int
+{
+  auto input = std::string();
+  auto stacks_init = std::vector<std::vector<char>>{};
+
+  while ( std::getline( std::cin, input ) )
+  {
+    if ( isdigit( input[1] ) != 0 )
+    {
+      break;
+    }
+    if ( stacks_init.empty() )
+    {
+      auto column_count = 1 + ( ( input.size() - 3 ) / 4 );
+      for ( auto i = size_t{ 0 }; i < column_count; ++i )
+      {
+        stacks_init.emplace_back( std::vector<char>{} );
+      }
+    }
+    for ( auto index = size_t{ 0 }; index < input.size(); index += 4 )
+    {
+      stacks_init[( index / 4 )].emplace_back( input[index + 1] );
+    }
+  }
+  // empty line
+  std::getline( std::cin, input );
+  auto stacks = std::vector<std::stack<char>>{};
+  for ( auto i = size_t{ 0 }; i < stacks_init.size(); ++i )
+  {
+    stacks.emplace_back( std::stack<char>{} );
+    for ( auto element = stacks_init[i].rbegin(); element != stacks_init[i].rend(); ++element )
+    {
+      if ( *element == ' ' )
+      {
+        break;
+      }
+      stacks[i].push( *element );
+    }
+  }
+
+  auto regexp = std::regex( R"(move (\d+) from (\d+) to (\d+))" );
+  while ( std::getline( std::cin, input ) )
+  {
+    std::cout << input << std::endl;
+    auto matches = std::smatch{};
+    std::regex_match( input, matches, regexp );
+    auto count = std::stoul( matches[1].str() );
+    auto from = std::stoul( matches[2].str() ) - 1;
+    auto to = std::stoul( matches[3].str() ) - 1;
+
+    // while(count != 0) {
+    //     auto element = stacks[from].top();
+    //     stacks[from].pop();
+    //     stacks[to].push(element);
+    //     --count;
+    // }
+    std::vector<char> temp;
+    while ( count != 0 )
+    {
+      auto element = stacks[from].top();
+      stacks[from].pop();
+      temp.push_back( element );
+      --count;
+    }
+    for ( auto element = temp.rbegin(); element != temp.rend(); ++element )
+    {
+      stacks[to].push( *element );
+    }
+  }
+
+  auto result = std::string{};
+  for ( auto pile : stacks )
+  {
+    result += pile.top();
+  }
+  std::cout << "Result: " << result << std::endl;
+
+  return 0;
+}
