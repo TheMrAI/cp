2ae3e60f88f0b24c66a91cae2b403c859ab1b53d
TheMrAI <mr.istvan.acs@gmail.com>
2023-04-25 18:11:44 +0200

    Add codeforces 867 solution subset

---
+++ codeforces/867/c.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using i6 = int64_t;
+
+// Source: https://codeforces.com/blog/entry/78852
+int ceil2(i6 a, i6 b) {
+    if (a == 0) return 0;
+    return ((a - 1)/b) + 1;
+}
+
+auto main() -> int {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    auto t=i6{0};
+    cin>>t;
+    while(t--) {
+        auto n = i6{0};
+        cin >> n;
+        cout << ((n-4)*(6+n))+26 << endl;
+    }
+
+    return 0;
+}
