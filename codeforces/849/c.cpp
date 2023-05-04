c8d7a5cb137fd5e53b638f937b93c2d3e43a298d
TheMrAI <mr.istvan.acs@gmail.com>
2023-05-04 14:52:34 +0200

    Add codeforces 849 solutions until e

---
+++ codeforces/849/c.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using i6 = int64_t;
+
+// Source: https://codeforces.com/blog/entry/78852
+int ceil2(i6 a, i6 b) {
+    if (a == 0) return 0;
+    return ((a - 1)/b) + 1;
+}
+
+auto main() -> int {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    auto t=i6{0};
+    cin>>t;
+    while(t--) {
+        auto n = 0;
+        cin >> n;
+        auto data = string{};
+        cin >> data;
+        auto l = 0;
+        auto h = data.size() - 1;
+        while(l < h) {
+            if(data[l] == data[h]) {
+                break;
+            }
+            ++l;
+            --h;
+        }
+        cout << data.size() - (2 * l) << endl;
+    }
+
+    return 0;
+}
---
+++ codeforces/849/e.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using i6 = int64_t;
+
+// Source: https://codeforces.com/blog/entry/78852
+int ceil2(i6 a, i6 b) {
+    if (a == 0) return 0;
+    return ((a - 1)/b) + 1;
+}
+
+auto main() -> int {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    auto t=i6{0};
+    cin>>t;
+    while(t--) {
+        auto n = 0;
+        cin>>n;
+        auto values = vector<i6>(n, 0);
+        auto negative_even_count = true; 
+        auto has_zero = false;
+        auto sum = i6{0};
+        for(auto i = 0; i < n; ++i) {
+            cin >> values[i];
+            if(values[i] < 0) {
+                negative_even_count = not negative_even_count;
+            }
+            if(values[i] == 0) {
+                has_zero = true;
+            }
+            values[i] = abs(values[i]);
+            sum += values[i];
+        }
+        if(has_zero || negative_even_count) {
+            cout << sum << endl;
+        } else {
+            ranges::sort(values);
+            cout << sum - 2*values[0] << endl;
+        }
+    }
+
+    return 0;
+}
