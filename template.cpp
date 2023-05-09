0ec749e5be5471a0287366849b943406df4fbfd0
TheMrAI <mr.istvan.acs@gmail.com>
2023-05-09 13:32:43 +0200

    Add dump_to function to template
    This helps with easier debugging. Added proper concepts too.

--- template.cpp
+++ template.cpp
@@ -3,8 +3,25 @@
 using namespace std;
 using i6 = int64_t;
 
+template<typename Output, typename Iter>
+requires 
+    std::derived_from<Output, std::ostream> &&
+    std::weakly_incrementable<Iter> && 
+    std::indirectly_readable<Iter>
+auto dump_to(Output& output, Iter begin, Iter end) {
+    while(begin != end) {
+        output << *begin;
+        if(std::next(begin) != end) {
+            output << ' ';
+        }
+        ++begin;
+    }
+}
+
 // Source: https://codeforces.com/blog/entry/78852
-int ceil2(i6 a, i6 b) {
+template<typename T>
+requires std::integral<T>
+auto ceil2(T a, T b) -> T {
     if (a == 0) return 0;
     return ((a - 1)/b) + 1;
 }
