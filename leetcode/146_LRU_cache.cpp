4853517be8b7728bb4eb60c2386a6fcc7df98085
TheMrAI <mr.istvan.acs@gmail.com>
2023-05-21 12:39:32 +0200

    Add a number of Leetcode solutions

---
+++ leetcode/146_LRU_cache.cpp
@@ -0,0 +1,69 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using i6 = int64_t;
+
+template<typename Output, typename Iter>
+requires 
+    std::derived_from<Output, std::ostream> &&
+    std::weakly_incrementable<Iter> && 
+    std::indirectly_readable<Iter>
+auto dump_to(Output& output, Iter begin, Iter end) {
+    while(begin != end) {
+        output << *begin;
+        if(std::next(begin) != end) {
+            output << ' ';
+        }
+        ++begin;
+    }
+}
+
+// Source: https://codeforces.com/blog/entry/78852
+template<typename T>
+requires std::integral<T>
+auto ceil2(T a, T b) -> T {
+    if (a == 0) return 0;
+    return ((a - 1)/b) + 1;
+}
+
+class LRUCache {
+private:
+    int capacity;
+    list<pair<int, int>> data_list;
+    unordered_map<int, list<pair<int, int>>::iterator> mapping;
+public:
+    LRUCache(int capacity_) : capacity{capacity_} 
+    {}
+    
+    int get(int key) {
+        auto match = mapping.find(key);
+        if(match == mapping.end()) {
+            return -1;
+        }
+        auto value = match->second->second;
+        data_list.erase(match->second);
+        data_list.emplace_front(key, value);
+        mapping[key] = data_list.begin();
+        return value;
+    }
+    
+    void put(int key, int value) {
+        auto match = mapping.find(key);
+        if(match != mapping.end()) {
+            data_list.erase(match->second);   
+        }
+        data_list.emplace_front(key, value);
+        mapping[key] = data_list.begin();
+        if(data_list.size() > capacity) {
+            mapping.erase(data_list.back().first);
+            data_list.pop_back();
+        }
+    }
+};
+
+auto main() -> int {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    return 0;
+}
---
+++ leetcode/42_trapping_rain_water.cpp
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using i6 = int64_t;
+
+template<typename Output, typename Iter>
+requires 
+    std::derived_from<Output, std::ostream> &&
+    std::weakly_incrementable<Iter> && 
+    std::indirectly_readable<Iter>
+auto dump_to(Output& output, Iter begin, Iter end) {
+    while(begin != end) {
+        output << *begin;
+        if(std::next(begin) != end) {
+            output << ' ';
+        }
+        ++begin;
+    }
+}
+
+// Source: https://codeforces.com/blog/entry/78852
+template<typename T>
+requires std::integral<T>
+auto ceil2(T a, T b) -> T {
+    if (a == 0) return 0;
+    return ((a - 1)/b) + 1;
+}
+
+class Solution {
+public:
+    int trap(vector<int>& heights) {
+        auto left_maximums = std::vector<int>(heights.size());
+        auto left_max = 0;
+        for (auto i = size_t{0}; i < heights.size(); ++i)
+        {
+            left_maximums[i] = left_max;
+            left_max = std::max(left_max, heights[i]);
+        }
+        
+        auto right_maximums = std::vector<int>(heights.size());
+        auto right_max = 0;
+        for (auto i = static_cast<int>(heights.size() - 1); i >= 0; --i)
+        {
+            right_maximums[i] = right_max;
+            right_max = std::max(right_max, heights[i]);
+        }
+        
+        auto total_volume = 0;
+        for (auto i = size_t{0}; i < heights.size(); ++i)
+        {
+            auto value = std::min(left_maximums[i], right_maximums[i]) - heights[i];
+            total_volume += std::max(value, 0);
+        }
+        
+        return total_volume;
+    }
+};
+
+auto main() -> int {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    return 0;
+}
