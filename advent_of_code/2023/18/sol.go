b7bc632036a39efd975c3bc8a98464b7010ac607
István Davorin Ács <mr.istvan.acs@gmail.com>
2023-12-18 20:51:00 +0100

    Progress on AOC/2023/18 part 2
    I made a skanline based solution and it is still buggy.
    It doesn't count some sections and definiately has some other bug
    because it panics if ran on the color parsed inputs. Uhh my eyeballs
    are melting.

--- advent_of_code/2023/18/sol.go
+++ advent_of_code/2023/18/sol.go
@@ -7,6 +7,7 @@ import (
 	"math"
 	"os"
 	"regexp"
+	"sort"
 	"strconv"
 )
 
@@ -68,26 +69,48 @@ type Pair struct {
 type LineSegment struct {
 	StartPoint Pair
 	LastPoint  Pair
-	Color      string
 }
 
-func DigDirectionsToLineSegments(startPoint Pair, digSteps []DigStep) []LineSegment {
+func (segment LineSegment) Length() int {
+	// this only works because we only have horizontal or vertical lines
+	// plus one adds back the end point
+	return 1 + Abs(segment.StartPoint.I-segment.LastPoint.I) + Abs(segment.StartPoint.J-segment.LastPoint.J)
+}
+
+func DigDirectionsToLineSegments(startPoint Pair, digSteps []DigStep, parseFromColor bool) []LineSegment {
 	lineSegments := []LineSegment{}
 
 	currentPoint := startPoint
 	for _, digStep := range digSteps {
 		lastPoint := currentPoint
-		switch digStep.Dir {
-		case Up:
-			lastPoint.I -= digStep.Count
-		case Right:
-			lastPoint.J += digStep.Count
-		case Down:
-			lastPoint.I += digStep.Count
-		case Left:
-			lastPoint.J -= digStep.Count
+		digDir := digStep.Dir
+		digCount := digStep.Count
+		lastCharIndex := len(digStep.Color) - 1
+		if parseFromColor {
+			dirValue, err := strconv.Atoi(digStep.Color[lastCharIndex:])
+			if err != nil {
+				log.Fatalf("Failed int conversion on input: '%v'\n", digStep.Color[lastCharIndex:])
+			}
+			// we shift the received value forward by one so it aligns with our ordering
+			digDir = Direction((dirValue + 1) % 4)
+			countValue, err := strconv.ParseInt(digStep.Color[1:lastCharIndex], 16, 64)
+			if err != nil {
+				log.Fatalf("Failed hexadecimal int conversion on input: '%v'\n", digStep.Color[1:lastCharIndex])
+			}
+			digCount = int(countValue)
 		}
-		lineSegments = append(lineSegments, LineSegment{currentPoint, lastPoint, digStep.Color})
+
+		switch digDir {
+		case Up:
+			lastPoint.I -= digCount
+		case Right:
+			lastPoint.J += digCount
+		case Down:
+			lastPoint.I += digCount
+		case Left:
+			lastPoint.J -= digCount
+		}
+		lineSegments = append(lineSegments, LineSegment{currentPoint, lastPoint})
 		currentPoint = lastPoint
 	}
 
@@ -108,7 +131,8 @@ func Max(lhs, rhs int) int {
 	return lhs
 }
 
-func PartOne(lineSegments []LineSegment) int {
+func PartOne(data []DigStep) int {
+	lineSegments := DigDirectionsToLineSegments(Pair{0, 0}, data, false)
 	matrix := LineSegmentsToMatrix(lineSegments)
 	for i := range matrix {
 		for j := range matrix[i] {
@@ -285,6 +309,163 @@ func Excavate(matrix [][]byte) [][]byte {
 	return excavated
 }
 
+type HorizontalSegment struct {
+	Line       LineSegment // just the height of the segment, can't be index, we may merge them
+	LeftIndex  int         // vertical segments can use the index
+	RightIndex int
+}
+
+// Alright all right, all right. Grug dev has to do complexity demon because there
+// isn't enough memory in the universe to load this abomination in.
+// What we do is a scanline solution.
+// Order the segments by coordinates, then scan from top to bottom.
+// On each horizontal section we decide what it does:
+// - if it is disjoint or extends previous ones, it builds
+// - if it is in the middle of a section it cuts
+func PartTwo(data []DigStep) int {
+	lineSegments := DigDirectionsToLineSegments(Pair{0, 0}, data, false)
+
+	horizontals := []HorizontalSegment{}
+	for i := range lineSegments {
+		fmt.Printf("i: %v, %v\n", i, lineSegments[i])
+		if lineSegments[i].StartPoint.I != lineSegments[i].LastPoint.I {
+			continue
+		}
+		prevIndex := len(lineSegments) - 1
+		if i > 0 {
+			prevIndex = i - 1
+		}
+		nextIndex := 0
+		if i < len(lineSegments)-1 {
+			nextIndex = i + 1
+		}
+		// based on how we built the line segments we know that next to a horizontal section there can only be
+		// two vertical ones
+		horizontal := LineSegment{lineSegments[i].StartPoint, lineSegments[i].LastPoint}
+		if horizontal.StartPoint.J > horizontal.LastPoint.J {
+			horizontal.StartPoint, horizontal.LastPoint = horizontal.LastPoint, horizontal.StartPoint
+		}
+		horizontals = append(horizontals, HorizontalSegment{horizontal, prevIndex, nextIndex})
+	}
+
+	sort.Slice(horizontals, func(i, j int) bool {
+		iHeight := horizontals[i].Line.StartPoint.I
+		jHeight := horizontals[j].Line.StartPoint.I
+		if iHeight == jHeight {
+			return horizontals[i].Line.StartPoint.J < horizontals[j].Line.StartPoint.J
+		}
+		return iHeight < jHeight
+	})
+
+	activeHorizontals, i := horizontalWithSameHeightFrom(horizontals, 0)
+	totalExcavated := 0
+	for i < len(horizontals) {
+		currentHeigh := horizontals[i].Line.StartPoint.I
+		for _, active := range activeHorizontals {
+			width := active.Line.Length() - 2
+			fmt.Printf("Width: %v\n", width)
+			height := Abs(active.Line.StartPoint.I-currentHeigh) - 1
+			fmt.Printf("Active: %v, currentHeight: %v\n", active, currentHeigh)
+			fmt.Printf("Excavated: %v\n", width*height)
+			totalExcavated += width * height
+		}
+
+		sameHeightSegments, sameHeightUntil := horizontalWithSameHeightFrom(horizontals, i)
+
+		zippedHorizontals := zipHorizontalSegmentSlices(activeHorizontals, sameHeightSegments)
+		// we must adjust all horizontal segments as if they were on the same height
+		for l := range zippedHorizontals {
+			zippedHorizontals[l].Line.StartPoint = Pair{currentHeigh, zippedHorizontals[l].Line.StartPoint.J}
+			zippedHorizontals[l].Line.LastPoint = Pair{currentHeigh, zippedHorizontals[l].Line.LastPoint.J}
+		}
+		fmt.Printf("Zipped: %v\n", zippedHorizontals)
+		// generate new activeHorizontals by merging or dividing existing ones
+		// this is made easier by ordering the horizontals on the j axis as well
+		nextActiveHorizontals := []HorizontalSegment{}
+		nextActiveHorizontals = append(nextActiveHorizontals, zippedHorizontals[0])
+		for zIndex := 1; zIndex < len(zippedHorizontals); zIndex++ {
+			lastIndex := len(nextActiveHorizontals) - 1
+			if nextActiveHorizontals[lastIndex].Line.LastPoint.J < zippedHorizontals[zIndex].Line.StartPoint.J {
+				// just add
+				nextActiveHorizontals = append(nextActiveHorizontals, zippedHorizontals[zIndex])
+			} else if nextActiveHorizontals[lastIndex].Line.LastPoint.J == zippedHorizontals[zIndex].Line.StartPoint.J {
+				// concatenate horizontal segment
+				nextActiveHorizontals[lastIndex].Line.LastPoint.J = zippedHorizontals[zIndex].Line.LastPoint.J
+				nextActiveHorizontals[lastIndex].RightIndex = zippedHorizontals[zIndex].RightIndex
+			} else if nextActiveHorizontals[lastIndex].Line.StartPoint.J <= zippedHorizontals[zIndex].Line.StartPoint.J && zippedHorizontals[zIndex].Line.LastPoint.J <= nextActiveHorizontals[lastIndex].Line.LastPoint.J {
+				// chop
+				firstPart := HorizontalSegment{LineSegment{nextActiveHorizontals[lastIndex].Line.StartPoint, zippedHorizontals[zIndex].Line.StartPoint}, nextActiveHorizontals[lastIndex].LeftIndex, zippedHorizontals[zIndex].LeftIndex}
+				secondPart := HorizontalSegment{LineSegment{zippedHorizontals[zIndex].Line.LastPoint, nextActiveHorizontals[lastIndex].Line.LastPoint}, zippedHorizontals[zIndex].RightIndex, nextActiveHorizontals[lastIndex].RightIndex}
+				nextActiveHorizontals = nextActiveHorizontals[:len(nextActiveHorizontals)-1]
+				if firstPart.Line.StartPoint.J != firstPart.Line.LastPoint.J {
+					nextActiveHorizontals = append(nextActiveHorizontals, firstPart)
+				}
+				if secondPart.Line.StartPoint.J != secondPart.Line.LastPoint.J {
+					nextActiveHorizontals = append(nextActiveHorizontals, secondPart)
+				}
+			} else {
+				log.Fatalf("Unexpected state achieved. Check logic.")
+			}
+		}
+		activeHorizontals = nextActiveHorizontals
+		i = sameHeightUntil
+	}
+
+	// add original trench to excavation total
+	for _, segment := range lineSegments {
+		totalExcavated += segment.Length()
+	}
+	// counted all endpoints twice, removing them once
+	totalExcavated -= len(lineSegments)
+	return totalExcavated
+}
+
+func horizontalWithSameHeightFrom(segments []HorizontalSegment, from int) ([]HorizontalSegment, int) {
+	sameHeightSegments := []HorizontalSegment{}
+	sameHeightIndex := from
+	sameHeightUntil := from + 1
+	for ; sameHeightUntil < len(segments) && segments[sameHeightUntil].Line.StartPoint.I == segments[sameHeightIndex].Line.StartPoint.I; sameHeightUntil++ {
+	}
+	sameHeightSegments = append(sameHeightSegments, segments[sameHeightIndex:sameHeightUntil]...)
+	return sameHeightSegments, sameHeightUntil
+}
+
+func zipHorizontalSegmentSlices(lhs []HorizontalSegment, rhs []HorizontalSegment) []HorizontalSegment {
+	zippedHorizontals := []HorizontalSegment{}
+	lhsIndex := 0
+	rhsIndex := 0
+	for lhsIndex < len(lhs) && rhsIndex < len(rhs) {
+		if lhs[lhsIndex].Line.StartPoint.J <= rhs[rhsIndex].Line.StartPoint.J {
+			zippedHorizontals = append(zippedHorizontals, lhs[lhsIndex])
+			lhsIndex++
+			continue
+		}
+		zippedHorizontals = append(zippedHorizontals, rhs[rhsIndex])
+		rhsIndex++
+	}
+	if lhsIndex < len(lhs) {
+		zippedHorizontals = append(zippedHorizontals, lhs[lhsIndex:]...)
+	}
+	if rhsIndex < len(rhs) {
+		zippedHorizontals = append(zippedHorizontals, rhs[rhsIndex:]...)
+	}
+	return zippedHorizontals
+}
+
+func Abs(val int) int {
+	if val < 0 {
+		return 0 - val
+	}
+	return val
+}
+
+func getJOnHeight(segment LineSegment, height int) int {
+	if segment.StartPoint.I == height {
+		return segment.StartPoint.J
+	}
+	return segment.LastPoint.J
+}
+
 func main() {
 	scanner := bufio.NewScanner(os.Stdin)
 	lines := []string{}
@@ -294,8 +475,9 @@ func main() {
 	}
 
 	data := ParseInput(lines)
-	lineSegments := DigDirectionsToLineSegments(Pair{0, 0}, data)
 
 	fmt.Println("Part one")
-	fmt.Printf("Total cubic meters excavated: %v\n", PartOne(lineSegments))
+	fmt.Printf("Total cubic meters excavated: %v\n", PartOne(data))
+	fmt.Println("Part two")
+	fmt.Printf("Total cubic meters excavated using colors for instructions: %v\n", PartTwo(data))
 }
