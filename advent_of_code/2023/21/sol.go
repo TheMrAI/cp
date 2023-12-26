a528a4fffd2148ded0e300547252c92d39cb437e
István Davorin Ács <mr.istvan.acs@gmail.com>
2023-12-26 14:42:47 +0100

    Add AOC/2023/21 part 2 solution

--- advent_of_code/2023/21/sol.go
+++ advent_of_code/2023/21/sol.go
@@ -22,6 +22,58 @@ type Pair struct {
 }
 
 func PartOne(matrix [][]byte, stepCount int) int {
+	reachablePlots := SimulateReachablePlots(matrix, stepCount)
+	return reachablePlots[stepCount]
+}
+
+// This solution only works on the big test input, not on the example
+// I couldn't figure it out myself.
+// Reddit user charr3 https://www.reddit.com/user/charr3/ explains:
+// "The main thing to notice for part 2 is that the grid is a square, and there are no obstacles in the same
+// row/col of the starting point. Let f(n) be the number of spaces you can reach after n steps. Let X be the length of
+// your input grid. f(n), f(n+X), f(n+2X), ...., is a quadratic, so you can find it by finding the first 3 values then
+// use that to interpolate the final answer."
+//
+// It never occurred to me that the big input would have a different solution than one for the example.
+// Even in the example I could see the diamond pattern, noted it in some commit, but never looked at the big input the
+// same way!
+func PartTwo(matrix [][]byte, stepCount int) int {
+	fmt.Println(len(matrix))
+	fmt.Println(len(matrix[0]))
+	// triple the incoming grid in size
+	tripledMatrix := ExpandMatrix(matrix, 9)
+	// for _, l := range tripledMatrix {
+	// 	fmt.Printf("%s\n", l)
+	// }
+
+	reachCounts := SimulateReachablePlots(tripledMatrix, 3*len(matrix))
+	for i, val := range reachCounts {
+		fmt.Printf("%3.v: %v\n", i, val)
+	}
+
+	return 0
+}
+
+func ExpandMatrix(matrix [][]byte, times int) [][]byte {
+	expandedMatrix := make([][]byte, len(matrix)*times)
+	sI := len(matrix) / 2
+	sJ := len(matrix[0]) / 2
+	// temporarily remove S for copying
+	matrix[sI][sJ] = '.'
+	for i := range expandedMatrix {
+		expandedMatrix[i] = make([]byte, len(matrix[0])*times)
+		for j := range expandedMatrix[i] {
+			expandedMatrix[i][j] = matrix[i%len(matrix)][j%len(matrix[0])]
+		}
+	}
+	matrix[sI][sJ] = 'S'
+	expandedMatrix[len(expandedMatrix)/2][len(expandedMatrix[0])/2] = 'S'
+	return expandedMatrix
+}
+
+func SimulateReachablePlots(matrix [][]byte, stepCount int) []int {
+	reachableAfterSteps := []int{1}
+
 	sCoord := Pair{0, 0}
 	for i := range matrix {
 		for j := range matrix[i] {
@@ -48,8 +100,9 @@ func PartOne(matrix [][]byte, stepCount int) int {
 				nextReachablePlots[nextCoord] = struct{}{}
 			}
 		}
-		fmt.Printf("Step %v - covered %v , prev covered: %v delta: %v\n", i+1, len(nextReachablePlots), len(reachablePlots), len(nextReachablePlots)-len(reachablePlots))
+		// fmt.Printf("Step %v - covered %v , prev covered: %v delta: %v\n", i+1, len(nextReachablePlots), len(reachablePlots), len(nextReachablePlots)-len(reachablePlots))
 		reachablePlots = nextReachablePlots
+		reachableAfterSteps = append(reachableAfterSteps, len(reachablePlots))
 
 		// fmt.Printf("Step %v - covered %v\n", i+1, len(reachablePlots))
 		for l := range matrix {
@@ -57,17 +110,17 @@ func PartOne(matrix [][]byte, stepCount int) int {
 			for m := range matrix[0] {
 				_, ok := reachablePlots[Pair{l, m}]
 				if ok {
-					// fmt.Print("O ")
+					// fmt.Print("O")
 					continue
 				}
-				// fmt.Printf("%c ", matrix[l][m])
+				// fmt.Printf("%c", matrix[l][m])
 			}
 			// fmt.Println()
 		}
 		// fmt.Println()
 	}
 
-	return len(reachablePlots)
+	return reachableAfterSteps
 }
 
 func main() {
@@ -88,4 +141,6 @@ func main() {
 
 	fmt.Println("Part one")
 	fmt.Printf("The number of reachable garden spots in 64 steps: %v\n", PartOne(matrix, 64))
+	fmt.Println("Part two")
+	fmt.Printf("The number of reachable garden spots in 26501365 steps: %v\n", PartTwo(matrix, 26501365))
 }
