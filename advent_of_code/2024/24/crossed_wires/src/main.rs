016a2f01d9a3f821c56cbabea7b26a224845e437
Istvan Davorin Acs <mr.istvan.acs@gmail.com>
2025-01-01 16:33:51 +0100

    Add AoC/2024/24 part two solution
    Well, not quite. Did the solution by hand, instead of coding up an
    automatic one. Time is too precious you know.
    The reason is that we know this has to be an optimal n digit adder and
    as the comments describe we know how it needs to look like.
    We know exactly what needs to happen and where. So from the first bit
    we can easily track and correct all mistakes on the first go.
    This however only works if you can identify exactly what do you see
    when you see an issue. This is realy not trivial to describe in code,
    but by doing it by hand it is relatively simple with the supplied
    support functions.
    Didn't like the solutions I have seen where it will simulate adding
    numbers, find a faulty place then switch things around. They run slowly
    and feel icky, so by hand it is.

--- advent_of_code/2024/24/crossed_wires/src/main.rs
+++ advent_of_code/2024/24/crossed_wires/src/main.rs
@@ -3,7 +3,7 @@ use std::io;
 
 use regex::Regex;
 
-#[derive(Debug, Copy, Clone)]
+#[derive(Debug, Copy, Clone, Eq, PartialEq, Hash)]
 enum GateTy {
     AND,
     OR,
@@ -62,6 +62,8 @@ fn parse_input() -> (Vec<(String, bool)>, Vec<Gate>) {
     (init_values, gates)
 }
 
+// In hindsight I see that this wasn't entirely necessary.
+// We could have done it with a simple DFS starting from the individual z bits.
 fn part_one(init_values: &[(String, bool)], ordered_gates: &[Gate]) -> usize {
     let mut value_of = HashMap::new();
     for init_value in init_values {
@@ -91,6 +93,7 @@ fn part_one(init_values: &[(String, bool)], ordered_gates: &[Gate]) -> usize {
             0
         };
     }
+
     decimal_number
 }
 
@@ -138,10 +141,110 @@ fn main() {
     let (init_values, gates) = parse_input();
 
     let gates = reorder_gates(&init_values, &gates);
+    for gate in &gates {
+        println!("{:?}", gate);
+    }
 
     println!(
         "The decimal number output on the wires is: {}",
         part_one(&init_values, &gates)
     );
+
+    let mut counter = HashMap::new();
+    for gate in &gates {
+        counter
+            .entry(gate.ty)
+            .and_modify(|count| *count += 1)
+            .or_insert(1);
+    }
+    for (k, v) in counter {
+        println!("{:?}: {}", k, v);
+    }
+
+    let mut tracer = HashMap::new();
+    for gate in &gates {
+        tracer.insert(
+            gate.result.clone(),
+            (gate.ty, [gate.lhs.clone(), gate.rhs.clone()]),
+        );
+    }
+    for i in (0..gates.len()).rev() {
+        if !gates[i].result.starts_with('z') {
+            break;
+        }
+        println!();
+        dfs(&tracer, &gates[i].result, 0);
+    }
     // println!("Similarity score: {}", part_two(&lhs, &rhs));
 }
+
+fn dfs(tracer: &HashMap<String, (GateTy, [String; 2])>, gate: &String, depth: usize) {
+    if depth > 3 {
+        return;
+    }
+    let mut tab = String::new();
+    for _ in 0..depth {
+        tab += " ";
+    }
+    if let Some(d) = tracer.get(gate) {
+        println!("{}{} {:?}", tab, gate, d.0);
+        dfs(tracer, &d.1[0], depth + 1);
+        dfs(tracer, &d.1[1], depth + 1);
+    } else {
+        println!("{}{}", tab, gate);
+    }
+}
+
+// A general one bit adder uses gates:
+// 2 XOR
+// 2 AND
+// 1 OR
+// for every single bit.
+// We have 45 bits 45*5 = 225
+// but because the first bit doesn't need the incoming carry bit logic
+// it can do be done with only 2 gates (XOR for the input and OR for the carry)
+// 225 - 3 = 222
+// And we have 222 gates in the input.
+// This means that we can be pretty sure that the input describes just an
+// optimal n bit adder (as we started with the minimal possible gate count necessary, there is no room
+// for shenanigans as the exercise says this adder should work on all inputs).
+// This means that we can pretty quickly figure out which gates are definitely incorrect
+// by going back the chain and looking for the expected pattern.
+
+// 0-th bit needs:
+// XOR
+//   x0
+//   y0
+
+// n-th bit needs:
+// XOR
+//  XOR
+//    x[n]
+//    y[n]
+//  OR
+//    AND
+//      (the same two legs as the top XOR from previous step, the carry in and xor of the inputs)
+//    AND
+//      x[n-1]
+//      y[n-1]
+
+// last bit needs (the carry out complex of the "previous" bit):
+//  OR
+//    AND
+//      (the same two legs as the top XOR from previous step)
+//    AND
+//      x[n-1]
+//      y[n-1]
+
+// AND: 89
+// XOR: 89
+// OR: 44
+
+// Gate { lhs: "y30", ty: AND, rhs: "x30", result: "rtq" }
+// Gate { lhs: "rmb", ty: AND, rhs: "rpt", result: "kcn" }
+// z06 - hwk
+// hpc - z31
+// cgr - z37
+// tnt - qmd
+
+// cgr,hpc,hwk,qmd,tnt,z06,z31,z37
