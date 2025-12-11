68f8374e7bea50ce693283d6546043f83b635bc2
Istvan Davorin Acs <mr.istvan.acs@gmail.com>
2025-12-11 10:54:09 +0100

    Add AOC/2025/10 part 1 solution
    Have a part 2 attempt, but it is vastly inefficient in solving
    the more complicated sections.
    Have to think a bit more, but we shall not use any lkinear algebra
    libraries like all the others... casuals..

--- advent_of_code/2025/10/src/main.zig
+++ advent_of_code/2025/10/src/main.zig
@@ -1,4 +1,5 @@
 const std = @import("std");
+const builtin = @import("builtin");
 
 const Machine = struct {
     start_state: u32,
@@ -78,7 +79,7 @@ pub fn main() !void {
     }
 
     std.debug.print("The fewest button presses required to correctly configure the indicator lights on all of the machines: {d}\n", .{try part_one(machines)});
-    // std.debug.print("The multiple of the X coordinate of the last two junction boxes: {d}\n", .{try part_two(allocator, junction_boxes, distances)});
+    std.debug.print("Tthe fewest button presses required to correctly configure the joltage level counters on all of the machines: {d}\n", .{try part_two(machines)});
 }
 
 fn part_one(machines: std.ArrayList(Machine)) error{OutOfMemory}!u32 {
@@ -91,6 +92,9 @@ fn part_one(machines: std.ArrayList(Machine)) error{OutOfMemory}!u32 {
     return configuration_steps;
 }
 
+// This is a BFS algorithm, because it doesn't need recursion and the possible search space is
+// small. For each machine the search space will be the 2^(# indicator lights). For my input
+// this seems to be max 2^10 == 1024, which is rather small.
 fn fewest_steps_to_match(desired_state: u32, operations: []u32) error{OutOfMemory}!u32 {
     var at_step: u32 = 1;
 
@@ -130,42 +134,103 @@ fn fewest_steps_to_match(desired_state: u32, operations: []u32) error{OutOfMemor
     return std.math.maxInt(u32);
 }
 
-// fn part_two(allocator: std.mem.Allocator, junction_boxes: std.ArrayList(Coordinate), distances: std.ArrayList(Distance)) error{OutOfMemory}!u64 {
-//     var dsu_parents = std.ArrayList(usize).empty;
-//     defer dsu_parents.deinit(allocator);
-//     try dsu_parents.appendNTimes(allocator, 0, junction_boxes.items.len);
-//     for (0..junction_boxes.items.len) |i| {
-//         make_set(dsu_parents, i);
-//     }
+fn part_two(machines: std.ArrayList(Machine)) error{OutOfMemory}!u32 {
+    var button_presses: u32 = 0;
 
-//     var dsu_ranks = std.ArrayList(usize).empty;
-//     defer dsu_ranks.deinit(allocator);
-//     try dsu_ranks.appendNTimes(allocator, 0, junction_boxes.items.len);
+    const allocator = std.heap.page_allocator;
+    for (machines.items) |machine| {
+        var seen_with_steps = std.HashMap([]u32, u32, SliceHashContext, std.hash_map.default_max_load_percentage).init(allocator);
+        defer seen_with_steps.deinit();
 
-//     var fully_connected = false;
-//     var distance_idx: usize = 0;
-//     while (!fully_connected) {
-//         const distance = distances.items[distance_idx];
-//         distance_idx += 1;
+        var current_joltages = std.ArrayList(u32).empty;
+        defer current_joltages.deinit(allocator);
+        try current_joltages.appendNTimes(allocator, 0, machine.jolts.items.len);
 
-//         union_sets(dsu_parents, dsu_ranks, distance.from, distance.to);
-//         // make sure to compress the tree
-//         for (0..dsu_parents.items.len) |i| {
-//             const p = find_set(dsu_parents, i);
-//             _ = p;
-//         }
-//         // inefficient check but hey, good enough, not in the mood to make it faster
-//         fully_connected = true;
-//         const value = dsu_parents.items[0];
-//         for (1..dsu_parents.items.len) |i| {
-//             if (dsu_parents.items[i] != value) {
-//                 fully_connected = false;
-//                 break;
-//             }
-//         }
-//     }
+        try fewest_button_presses_for_joltage_dfs(allocator, &seen_with_steps, machine.jolts.items, machine.buttons.items, current_joltages, 0);
 
-//     // distance_idx-1 because the loop iterates it before ending
-//     const last_connection = distances.items[distance_idx - 1];
-//     return @as(u64, @intFromFloat(junction_boxes.items[last_connection.from].x)) * @as(u64, @intFromFloat(junction_boxes.items[last_connection.to].x));
-// }
+        button_presses += seen_with_steps.get(machine.jolts.items).?;
+    }
+
+    return button_presses;
+}
+
+// The maximum button press needed for joltage targets is a few hundred (~300) and there are a about maximum 10
+// numbers for a given machine, which means that in the worst case we have 300^10 possible combinations
+// if we just wanted to brute force this. The number is btw: 5,904,900,000,000,000,000,000,000 (can't even pronounce it
+// so brute won't work)
+// Then we can look it perhaps differently at the problem.
+// There are these buttons witch specify how the N counters are increased, and there are M buttons.
+// If we look at the problem like this, then for example:
+// [.##.] (3) (1,3) (2) (2,3) (0,2) (0,1) {3,5,4,7} means that we have
+// (3) => {0, 0, 0, 1}*A1
+// (1,3) => {0, 1, 0, 1}*A2
+// (2) => {0, 0, 1, 0}*A3
+// (2,3) => {0, 0, 1, 1}*A4
+// (0,2) => {1, 0, 1, 0}*A5
+// (0,1) => {1, 1, 0, 0}*A6
+// And then we are looking for sum(Ax) such that it is minimal and sum(Ax) = {3,5,4,7}.
+// Notice we aren't interested in any specific A value, only that for a given sum A, can we get the target.
+// Which makes me wonder if we can state the problem as
+// can_you_solve_it_with_exactly_this_sum_for_target
+// and then binary search the space. With lower bound being 1 and upper bound being the sum of the jolts.
+// This could maybe be good as log(SumA) for almost any number will be trivially small, but not sure how we
+// can estimate solving the problem for a given number.
+// Well, no matter how much I think it seems that solving for one given number too is extremely complicated,
+// as you have to basically check all multiples for each button within between 0 and the smallest value it
+// has to provide in the final joltage.
+fn fewest_button_presses_for_joltage_dfs(allocator: std.mem.Allocator, seen_with_steps: *std.HashMap([]u32, u32, SliceHashContext, std.hash_map.default_max_load_percentage), joltages: []u32, operations: []u32, current_joltages: std.ArrayList(u32), current_step: u32) error{OutOfMemory}!void {
+    for (0..joltages.len) |i| {
+        if (current_joltages.items[i] > joltages[i]) {
+            return;
+        }
+    }
+    if (seen_with_steps.contains(current_joltages.items)) {
+        const seen_at = seen_with_steps.get(current_joltages.items).?;
+        if (seen_at <= current_step) {
+            return;
+        }
+    }
+    try seen_with_steps.put(current_joltages.items, current_step);
+
+    for (operations) |operation| {
+        // std.debug.print("ITER\n", .{});
+        var next_joltages = try current_joltages.clone(allocator);
+
+        var increment_indicator = operation;
+        for (0..joltages.len) |i| {
+            if (increment_indicator & 1 == 1) {
+                next_joltages.items[joltages.len - 1 - i] += 1;
+            }
+            increment_indicator >>= 1;
+        }
+
+        // std.debug.print("Dumping keys:\n", .{});
+        // var iter = seen.keyIterator();
+        // while (iter.next()) |key| {
+        //     std.debug.print("{any}\n", .{key.*});
+        // }
+        // std.debug.print("\nCandidate: {any}\n", .{next_press_counts.items});
+
+        try fewest_button_presses_for_joltage_dfs(allocator, seen_with_steps, joltages, operations, next_joltages, current_step + 1);
+        // std.debug.print("put\n", .{});
+    }
+}
+
+const SliceHashContext = struct {
+    pub fn hash(_: SliceHashContext, counters: []u32) u64 {
+        var h = std.hash.Wyhash.init(0);
+        for (counters) |counter| {
+            var as_bytes: [4]u8 = undefined;
+            std.mem.writeInt(u32, &as_bytes, counter, builtin.cpu.arch.endian());
+            h.update(&as_bytes);
+        }
+        const hashs = h.final();
+        // std.debug.print("{any} {}\n", .{ counters, hashs });
+
+        return hashs;
+    }
+
+    pub fn eql(_: SliceHashContext, lhs: []u32, rhs: []u32) bool {
+        return std.mem.eql(u32, lhs, rhs);
+    }
+};
